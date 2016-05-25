ace3ca9dd164a72a08e0104cf4db0f1553e4265f
Grace <grace.h.gamble@gmail.com>
2016-05-25 21:14:08 +0000

    Second Update to JS4 Victims and Volunteers

--- victimsvolunteers.js
+++ victimsvolunteers.js
@@ -2,13 +2,46 @@
 var victimNumber = prompt("How many disaster victims do you wish to enter?");
 
 //Step Two
-var increment = 1;
+var incrementVictim = 1;
+var allVictims = [];
 
-do {
-    var newVictim = prompt("Please enter the victim's name, phone number, and address.");
-    increment ++;
+if (victimNumber > 0) {
+    do {
+        var newVictim = prompt("Please enter the victim's name, phone number, and street.");
+        incrementVictim ++;
+        var resVictim = newVictim.split(","); 
+        console.log(resVictim);
+        allVictims.push(resVictim);
+    }
+    while (incrementVictim <= victimNumber);
 }
-while (increment <= victimNumber);
+else {
+    alert("Thanks!");
+}
 
-newVictim.push
+//Step Three
+var volunteerNumber = prompt("How many disaster volunteers do you wish to enter?");
+var incrementVolunteer = 1;
+var allVolunteers = [];
+
+if (volunteerNumber > 0) {
+    do {
+        var newVolunteer = prompt("Please enter volunteer's name, phone number, and street.");
+        incrementVolunteer ++;
+        var resVolunteer = newVolunteer.split(",");
+        console.log(resVolunteer);
+        allVolunteers.push(resVolunteer);
+    }
+    while (incrementVolunteer <= volunteerNumber);
+}
+else {
+    alert("Thanks!");
+}
+
+//Step Four
+allVictims.toString;
+allVolunteers.toString;
+
+alert("Your victims are" + " " + allVictims + " " + "and your volunteers are" + " " + allVolunteers);
+
 
