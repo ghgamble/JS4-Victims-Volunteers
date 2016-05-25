1f2423c165c529ba2c310052379e0bf673213df1
Grace <grace.h.gamble@gmail.com>
2016-05-25 21:45:35 +0000

    Bonus 1 to JS4 Victims and Volunteers

---
+++ bonus1.js
@@ -0,0 +1,7 @@
+do {
+    var newVictim = prompt("Please enter the victim's name, phone number, and street");
+    newVictim ++;
+    var addAnotherVictim = confirm("Would you like to continue adding more victims?");
+}
+
+while (addAnotherVictim === true); 
