4a593d6c3225a43e777b80187a8faf34acf221f1
Grace <grace.h.gamble@gmail.com>
2016-05-25 18:17:30 +0000

    First Update to JS4 Victims and Volunteers

---
+++ victimsvolunteers.js
@@ -0,0 +1,14 @@
+//Step One
+var victimNumber = prompt("How many disaster victims do you wish to enter?");
+
+//Step Two
+var increment = 1;
+
+do {
+    var newVictim = prompt("Please enter the victim's name, phone number, and address.");
+    increment ++;
+}
+while (increment <= victimNumber);
+
+newVictim.push
+
